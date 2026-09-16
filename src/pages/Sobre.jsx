a64c0da2b1fb79c959f8be66b8a361bf7a3c8b2a
emanuelbrum512 <emanuelbrum512@gmail.com>
2026-09-16 09:41:06 -0300

    Sobre

---
+++ src/pages/Sobre.jsx
@@ -0,0 +1,16 @@
+function Sobre() {
+  return (
+    <div className="sobre-container">
+      <h1 className="titulo-pagina">Sobre o Empório Geek</h1>
+
+      <p>Este é um projeto feito em dupla, utilizando React + Vite.</p>
+      <p>
+        Ele utiliza uma api API criada para exibir um catálogo de produtos Geek,
+        permitindo buscar por nome, filtrar por categorias e navegar entre
+        páginas.
+      </p>
+    </div>
+  );
+}
+
+export default Sobre;
